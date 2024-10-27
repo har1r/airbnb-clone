17858005cf7721c3bfba90e32171345e52cd3d46
har1r <muftiharir3@gmail.com>
2024-10-28 06:04:39 +0700

    Membuat Layout, Header, dan fitur Login website

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
+import Layout from './components/Layout'
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<IndexPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+      </Route>
+      
+    </Routes>
+  )
+}
+
+export default App
