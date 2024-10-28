64181858539bb2bdc5e2ea0342c373771ef3b895
unknown <muftiharir3@gmail.com>
2024-10-28 16:34:20 +0700

    Melengkapi fitur login, registrasi

---
+++ api/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const User = require('./models/User.js');
+const bycript = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+const bycriptSalt = bycript.genSaltSync(10);
+const jwtSecret = 'sdfaskjhf626woigfagd24sghkag';
+
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173'
+}));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL);
+
+// Path for resigter, Login
+app.post('/register', async (req, res) => {
+    const {name, email, password} = req.body;
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bycript.hashSync(password, bycriptSalt)
+        });
+        res.json(userDoc);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+    const userDoc = await User.findOne({email});
+    if (userDoc) {
+        const passOk = bycript.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token).json('pass ok');
+            });
+        } else {
+            res.status(422).json('pass not ok');
+        }
+    } else {
+        res.json(0);
+    }
+});
+
+app.get('/test', (req, res) => res.send('Hello World!'));
+app.listen(3000, () => console.log('server running on port 3000!'));
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import './App.css'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import Layout from './components/Layout'
+import RegisterPage from './pages/RegisterPage'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
@@ -10,6 +15,7 @@ function App() {
       <Route path='/' element={<Layout/>}>
         <Route index element={<IndexPage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
       </Route>
       
     </Routes>
