2b093c0306de90ddd7582ddeffbc4ae9a57109a1
har1r <muftiharir3@gmail.com>
2024-11-07 16:25:51 +0700

    merapikan fitur kirim file

--- api/controllers/placeController.js
+++ api/controllers/placeController.js
@@ -1,4 +1,5 @@
 const imageDownloader = require('image-downloader');
+const fs = require('fs');
 
 class placeController{
     static async linkPhotoUpload(req, res) {
@@ -12,7 +13,16 @@ class placeController{
     };
 
     static photoUpload(req, res) {
-        res.json(req.files);
+        const uploadedFiles = [];
+        for(let i = 0; i < req.files.length; i++) {
+            const {path, originalname} = req.files[i];
+            const parts = originalname.split('.');
+            const ext = parts[parts.length - 1];
+            const newPath = path + '.' + ext;
+            fs.renameSync(path, newPath);
+            uploadedFiles.push(newPath.replace('uploads\\', ''));
+        }
+        res.json(uploadedFiles);
     }
 };
 
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -43,13 +43,15 @@ export default function PlacesPage() {
     ev.preventDefault();
     const files = ev.target.files;
     const data = new FormData();
-    data.set('photos', files);
+    for(let i = 0; i < files.length; i++) {
+      data.append('photos', files[i]);
+    }
     axios.post("/place/upload", data, {
       headers: {'Content-Type': 'multipart/form-data'}
     }).then(response => {
-      const {data:filename} = response;
+      const {data:filenames} = response;
       setAddedPhotos((prev) => {
-        return [...prev, filename];
+        return [...prev, ...filenames];
       });
     })
   };
@@ -118,15 +120,15 @@ export default function PlacesPage() {
             <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
               {addedPhotos.length > 0 &&
                 addedPhotos.map((link) => (
-                  <div>
+                  <div className="h-32 flex">
                     <img
-                      className="rounded-2xl"
+                      className="rounded-2xl w-full object-cover"
                       src={"http://localhost:3000/uploads/" + link}
                       alt=""
                     />
                   </div>
                 ))}
-              <label className="cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+              <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
                 <input type="file" className="hidden" onChange={uploadPhoto}/>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
